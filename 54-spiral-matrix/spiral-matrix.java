b9f0cb3dfb62aef17134dcec7ecb2d0ec1799cf1
Ashish Murarka <murarkaashish6294@gmail.com>
2026-02-04 22:42:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (6.18%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -5,30 +5,39 @@ class Solution {
         int cols = matrix[0].length;
         int top = 0;
         int bottom = rows - 1;
-        int left =  0;
+        int left = 0;
         int right = cols - 1;
-        while(left <= right && top <= bottom){
-            for(int i = left; i <= right; i++){
-                list.add(matrix[top][i]);
+        while(true){
+        for (int i = left; i <= right; i++) {
+            list.add(matrix[top][i]);
+            if (list.size() == rows * cols) {
+                return list;
             }
-            top++;
-            for(int i = top; i <= bottom; i++){
-                list.add(matrix[i][right]);
-            }
-            right--;
-            if(top <= bottom){
-                for(int i = right; i >= left; i--){
-                    list.add(matrix[bottom][i]);
-                }
-            }
-            bottom--;
-            if(left <= right){
-                for(int i = bottom; i >= top; i--){
-                    list.add(matrix[i][left]);
-                }
-            }
-            left++;
         }
-        return list;
+        top++;
+        for (int i = top; i <= bottom; i++) {
+            list.add(matrix[i][right]);
+            if (list.size() == rows * cols) {
+                return list;
+            }
+        }
+        right--;
+
+        for (int i = right; i >= left; i--) {
+            list.add(matrix[bottom][i]);
+            if (list.size() == rows * cols) {
+                return list;
+            }
+        }
+        bottom--;
+        for (int i = bottom; i >= top; i--) {
+            list.add(matrix[i][left]);
+            if (list.size() == rows * cols) {
+                return list;
+            }
+
+        }
+        left++;
+    }
     }
 }
