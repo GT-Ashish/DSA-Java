414e53917f433f870b2c02da8d1c0365562709ed
Ashish Murarka <murarkaashish6294@gmail.com>
2026-02-08 13:19:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (27.12%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,27 +1,20 @@
 class Solution {
     public String interpret(String command) {
-       int length = command.length();
-       StringBuilder sb = new StringBuilder();
-       for(int i = 0; i < length; i++){
-        char ch = command.charAt(i);
-        if(ch == 'G'){
-            sb.append('G');
-        }
-        else{
-            if(ch == '('){
-                if(i + 1 < length && command.charAt(i + 1) == ')'){
-                    sb.append('o');
-                    i++;
-                }
-                else{
-                    if(i + 1 < length && i + 2 < length && i + 3 < length && command.charAt(i + 1) == 'a' && command.charAt(i + 2) == 'l' && command.charAt(i + 3) == ')'){
-                        sb.append("al");
-                        i += 3;
-                    }
-                }
+        int length = command.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            char ch = command.charAt(i);
+            if (ch == 'G') {
+                sb.append('G');
+            } else if (ch == '(' && i + 1 < length && command.charAt(i + 1) == ')') {
+                sb.append('o');
+                i++;
+            } else {
+                //as the question clearly states the command can have threee kinds of values so if its not the two above it must be this so no need to check.
+                sb.append("al");
+                i += 3;
             }
         }
-       }
-       return sb.toString();
+        return sb.toString();
     }
 }
